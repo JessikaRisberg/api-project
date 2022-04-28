6a34f8adc6f473ce00ca53de1723dbf8b7be86eb
Jessika Risberg <33091640+JessikaRisberg@users.noreply.github.com>
2022-04-28 10:59:41 +0200

    desgin of login/signup site done (no functionality)

--- src/app.py
+++ src/app.py
@@ -1,11 +1,18 @@
 from flask import Flask
-app = Flask(__name__)
 
 
-@app.route("/")
-def main():
-    return "H3ll0 :)"
+
+def create_app():
+    app = Flask(__name__)
+
+    # Register the open blueprint with app object
+    from blueprints.open import bp_open
+    app.register_blueprint(bp_open)
+
+
+    return app
 
 
 if __name__ == "__main__":
-  app.run()
+    app = create_app()
+    app.run()
