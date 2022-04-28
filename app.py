e785d276fcbe53f245d0d8a0dc5c85e01e9718ca
stoffeen <christoffer.olsson@student.kyh.se>
2022-04-28 10:08:11 +0200

    -

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, request, url_for, redirect
+from flask_pymongo import PyMongo
+
 app = Flask(__name__)
+app.config['MONGO_URI'] = 'mongodb+srv://Christoffer:hvsxYJ3GOVtjeMK3@cluster0.cjztj.mongodb.net/test'
+mongo = PyMongo(app)
 
 
-@app.route("/")
-def main():
-    return "H3ll0 :)"
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-
-if __name__ == "__main__":
-  app.run()
+if __name__ == '__main__':
+    app.run()
