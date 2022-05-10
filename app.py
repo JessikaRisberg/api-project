5e0265e9f60025c5e7db772132b4b603484b1b59
Your Namesaiokokun <grigory.backman@student.kyh.se>
2022-05-10 20:54:02 +0200

    Added CRUD functionality for REST API calls

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
 import logging
-from flask import Flask, request, jsonify, make_response
+import sqlite3
+from flask import Flask, request, jsonify, make_response, Response
 from flask_sqlalchemy import SQLAlchemy
 import uuid
 from werkzeug.security import generate_password_hash, check_password_hash
 import jwt
 import datetime
 from functools import wraps
-from models import db, Log, User
+from models import db, Log, User, update_dog, ShelterDogs
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'securekey'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
-
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
@@ -36,13 +37,13 @@ def token_required(f):
             token = request.headers['x-access-token']
 
         if not token:
-            return jsonify({'message' : 'No token.'}), 401
+            return jsonify({'message': 'No token.'}), 401
 
         try:
             data = jwt.decode(token, app.config['SECRET-KEY'])
             current_user = User.query.filter_by(public_id=data['public_id']).first()
         except:
-            return jsonify({'message' : 'Token is invalid'}), 401
+            return jsonify({'message': 'Token is invalid'}), 401
 
         return f(current_user, *args, **kwargs)
 
@@ -52,9 +53,8 @@ def token_required(f):
 @app.route('/user', methods=['GET'])
 @token_required
 def get_all_users(current_user):
-
     if not current_user.admin:
-        return jsonify({'message' : "Can't perform that function"})
+        return jsonify({'message': "Can't perform that function"})
 
     users = User.query.all()
 
@@ -73,15 +73,13 @@ def get_all_users(current_user):
 @app.route('/user/<public_id>', methods=['GET'])
 @token_required
 def get_one_user(current_user, public_id):
-
     if not current_user.admin:
-        return jsonify({'message' : "Can't perform that function"})
+        return jsonify({'message': "Can't perform that function"})
 
     user = User.query.filter_by(public_id=public_id).first()
 
     if not user:
         return jsonify({'message': 'No user found'})
-
 
     user_data = {}
     user_data['public_id'] = user.public_id
@@ -96,7 +94,7 @@ def get_one_user(current_user, public_id):
 @token_required
 def create_user(current_user):
     if not current_user.admin:
-        return jsonify({'message' : "Can't perform that function"})
+        return jsonify({'message': "Can't perform that function"})
 
     data = request.get_json()
 
@@ -113,7 +111,7 @@ def create_user(current_user):
 @token_required
 def promote_user(current_user, public_id):
     if not current_user.admin:
-        return jsonify({'message' : "Can't perform that function"})
+        return jsonify({'message': "Can't perform that function"})
 
     user = User.query.filter_by(public_id=public_id).first()
 
@@ -156,11 +154,114 @@ def login():
         return make_response('Could not verify', 401, {'WWW-Authenticate', 'Basic realm="Login required!"'})
 
     if check_password_hash(user.password, auth.password):
-        token = jwt.encode({'public_id': user.public_id, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
+        token = jwt.encode(
+            {'public_id': user.public_id, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)},
+            app.config['SECRET_KEY'])
 
-        return jsonify({'token' : token.encode().decode('UTF-8')})
+        return jsonify({'token': token.encode().decode('UTF-8')})
 
     return make_response('Could not verify', 401, {'WWW-Authenticate', 'Basic realm="Login required!"'})
+
+
+@app.route("/dog/create", methods=['POST'])
+def createdog():
+    sqliteconnection = sqlite3.connect('db.sqlite')
+    cursor = sqliteconnection.cursor()
+
+    cursor.execute('SELECT ID FROM shelter_dogs WHERE ID IN ( SELECT max( ID ) FROM shelter_dogs );')
+    row = cursor.fetchone()
+    rownumb = row[0] + 1
+
+    dogdata = request.get_json()
+
+    new_dog = ShelterDogs(ID=rownumb, name=dogdata["name"], age=dogdata["age"], sex=dogdata["sex"],
+                          breed=dogdata["breed"], color=dogdata["color"], coat=dogdata["coat"],
+                          size=dogdata["size"],
+                          neutered=dogdata["neutered"], likes_children=dogdata["likes_children"])
+    db.session.add(new_dog)
+    db.session.commit()
+
+    return jsonify({'message: New dog created with the index:': rownumb})
+
+
+@app.route("/dog/read", methods=["GET"])
+def readdog():
+    dogs = ShelterDogs.query.all()
+
+    output = []
+
+    for filtdog in dogs:
+        dogs_data = {'ID': filtdog.ID, 'name': filtdog.name, 'age': filtdog.age,
+                     'breed': filtdog.breed}
+        output.append(dogs_data)
+    return jsonify({'dogs': output})
+
+
+@app.route("/dog/read/<name>", methods=["PUT"])
+def readsomedogs(name):
+    try:
+        sqliteconnection = sqlite3.connect('db.sqlite')
+        cursor = sqliteconnection.cursor()
+
+        sql_select_query = """select * from shelter_dogs where name = ?"""
+        cursor.execute(sql_select_query, (name,))
+        records = cursor.fetchall()
+        output = []
+        for row in records:
+            dogsearch = f'index: {row[0]}, name: {row[1]}, age: {row[2]}, sex: {row[3]}, breed: {row[4]},' \
+                        f' color: {row[5]}, coat: {row[6]}, size: {row[7]}, neutered: {row[8]}, likes_children: {row[9]}'
+            output.append(dogsearch)
+
+        return jsonify({'You searched for': name}, {"Here are the results": output})
+        cursor.close()
+
+    except sqlite3.Error as error:
+        return jsonify("Failed to read data", error)
+    finally:
+        if sqliteConnection:
+            sqliteConnection.close()
+
+
+@app.route("/dog/update", methods=["PUT"])
+def updatedog():
+    request_data = request.get_json()
+    update_dog(request_data['ID'], request_data['name'], request_data['age'], request_data["sex"],
+               request_data['breed'],
+               request_data['color'], request_data['coat'], request_data['size'], request_data['neutered'],
+               request_data['likes_children'])
+    response = Response("Dog Updated", status=200, mimetype='application/json')
+    return response
+
+
+@app.route("/dog/delete/<id>", methods=["DELETE"])
+def deletedog(id):
+    try:
+        sqliteconnection = sqlite3.connect('db.sqlite')
+        cursor = sqliteconnection.cursor()
+        sql_select_query = """select * from shelter_dogs where ID = ?"""
+        cursor.execute(sql_select_query, (id,))
+        records = cursor.fetchall()
+        output = []
+        for row in records:
+            dogsearch = f'index: {row[0]}, name: {row[1]}, age: {row[2]}, sex: {row[3]}, breed: {row[4]},' \
+                        f' color: {row[5]}, coat: {row[6]}, size: {row[7]}, neutered: {row[8]}, likes_children: {row[9]}'
+            output.append(dogsearch)
+        sql_delete_query = """DELETE from shelter_dogs where ID = ?"""
+        cursor.execute(sql_delete_query, (id,))
+        sqliteconnection.commit()
+        cursor.close()
+        return jsonify({"You have chosen to delete": output}, "Dog deleted successfully")
+
+    except sqlite3.Error as error:
+        return jsonify("Failed to read data", error)
+    finally:
+        if sqliteconnection:
+            sqliteconnection.close()
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    return "500 error - Internal Server Exception \n Did you write everything correctly?", 500
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -18,3 +18,30 @@ class Log(db.Model):
     endpoint = db.Column(db.String)
     user = db.Column(db.String)
     timestamp = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+
+
+def update_dog(_ID, _name, _age, _sex, _breed, _color, _coat, _size, _neutered, _likes_children):
+    dog_to_update = ShelterDogs.query.filter_by(ID=_ID).first()
+    dog_to_update.name = _name
+    dog_to_update.age = _age
+    dog_to_update.sex = _sex
+    dog_to_update.breed = _breed
+    dog_to_update.color = _color
+    dog_to_update.coat = _coat
+    dog_to_update.size = _size
+    dog_to_update.neutered = _neutered
+    dog_to_update.likes_children = _likes_children
+    db.session.commit()
+
+
+class ShelterDogs(db.Model):
+    ID = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    age = db.Column(db.Integer)
+    sex = db.Column(db.String)
+    breed = db.Column(db.String)
+    color = db.Column(db.String)
+    coat = db.Column(db.String)
+    size = db.Column(db.String)
+    neutered = db.Column(db.String)
+    likes_children = db.Column(db.String)
